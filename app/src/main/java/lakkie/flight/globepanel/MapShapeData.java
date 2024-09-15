03ed5d70f1fee7588e7dbad6464b4ad4c2a21120
lakkiegaming@gmail.com <lakkiegaming@gmail.com>
2024-09-15 01:35:22 -0700

    Able to read new world file format (not working entirely)

--- app/src/main/java/lakkie/flight/globepanel/MapShapeData.java
+++ app/src/main/java/lakkie/flight/globepanel/MapShapeData.java
@@ -8,11 +8,21 @@ import java.util.Scanner;
 
 public class MapShapeData {
 
+    private static final float POLYGON_SCALE = 10.f;
+
+    /**
+     * The offset of all xPoints and yPoints in the shape.
+     */
     public float x = 0, y = 0;
+    /**
+     * The total number of points in the polygon. Always equal to xPoints.length or yPoints.length.
+     */
     public final int numPoints;
+    /**
+     * Points in the polygon. May be relative to each other or absolute positions depending on this.relativePoints
+     */
     private float[] xPoints, yPoints;
     private Polygon polygon = null;
-
     /**
      * Whether all the points in the xPoints and yPoints arrays are relative to each other.
      */
@@ -26,34 +36,45 @@ public class MapShapeData {
         this.yPoints = new float[numPoints];
     }
 
-    private void updatePolygon() {
-        // Must be at least 10 if you don't want loss of data because the data goes to tenths place
-        final float polygonScale = 10.f;
-        int[] absPointsX = new int[numPoints];
-        int[] absPointsY = new int[numPoints];
-        int currentX = (int) Math.floor(x * polygonScale);
-        int currentY = (int) Math.floor(y * polygonScale);
+    private void updatePolygonRelative() {
+        int[] polyPointsX = new int[numPoints];
+        int[] polyPointsY = new int[numPoints];
+        // Start at the offset position
+        int currentXScaled = (int) Math.floor(this.x * POLYGON_SCALE);
+        int currentYScaled = (int) Math.floor(this.y * POLYGON_SCALE);
+
         for (int i = 0; i < numPoints; i++) {
-            if (relativePoints) {
-                /*
-                * For this mode, each point is relative to and builds on the last point.
-                * For example, a line might contain 2 points in the raw data: -1.2 0.3 -2.9 1
-                * But to draw as a Java polygon, we have to keep a running position of where we're at and add our current point
-                * to that. So we start at (-1.2, 0.3) and the next point is (-1.2 + -2.9, 0.3 + 1)
-                */
-                currentX += (int) Math.floor(polygonScale * xPoints[i]);
-                currentY += (int) Math.floor(polygonScale * yPoints[i]);
-                absPointsX[i] = currentX;
-                absPointsY[i] = currentY;
-            } else {
-                // Absolute points, treat currentX and currentY as the starting point and add our current point to that
-                // We do not modify currentX and currentY
-                absPointsX[i] = (int) Math.floor(polygonScale * xPoints[i]) + currentX;
-                absPointsY[i] = (int) Math.floor(polygonScale * yPoints[i]) + currentY;
-            }
+            currentXScaled += (int) Math.floor(POLYGON_SCALE * xPoints[i]);
+            currentYScaled += (int) Math.floor(POLYGON_SCALE * yPoints[i]);
+            
+            polyPointsX[i] = currentXScaled;
+            polyPointsY[i] = currentYScaled;
         }
 
-        this.polygon = new Polygon(absPointsX, absPointsY, numPoints);
+        this.polygon = new Polygon(polyPointsX, polyPointsY, numPoints);
+    }
+
+    private void updatePolygonAbsolute() {
+        int[] polyPointsX = new int[numPoints];
+        int[] polyPointsY = new int[numPoints];
+        int offsetXScaled = (int) Math.floor(this.x * POLYGON_SCALE);
+        int offsetYScaled = (int) Math.floor(this.y * POLYGON_SCALE);
+        for (int i = 0; i < numPoints; i++) {
+            // Absolute points, treat currentX and currentY as the starting point and add our current point to that
+            // We do not modify currentX and currentY
+            polyPointsX[i] = (int) Math.floor(POLYGON_SCALE * xPoints[i]) + offsetXScaled;
+            polyPointsY[i] = (int) Math.floor(POLYGON_SCALE * yPoints[i]) + offsetYScaled;
+        }
+
+        this.polygon = new Polygon(polyPointsX, polyPointsY, numPoints);
+    }
+
+    private void updatePolygon() {
+        if (this.relativePoints) {
+            updatePolygonRelative();
+        } else {
+            updatePolygonAbsolute();
+        }
     }
 
     public Polygon getPolygon() {
@@ -75,67 +96,65 @@ public class MapShapeData {
         this.yPoints[idx] = y;
     }
 
-    public static MapShapeData parseRelativePolygon(Scanner input) {
-        float posX = input.nextFloat();
-        float posY = input.nextFloat();
-        int numPoints = input.nextInt();
+    public static MapShapeData parsePolygon(String shape, boolean relativePoints) {
+        List<Float> xPoints = new ArrayList<Float>();
+        List<Float> yPoints = new ArrayList<Float>();
 
-        MapShapeData country = new MapShapeData(posX, posY, numPoints, true);
-
-        int pointIdx = 0;
-        while (input.hasNext()) {
-            float x = input.nextFloat();
-            float y = input.nextFloat();
-            country.setPoint(pointIdx++, x, y);
+        String[] shapePoints = shape.split("\\s+");
+        if (shapePoints.length < 3) {
+            throw new IllegalArgumentException("Map shape must have at least 3 points");
         }
 
-        country.updatePolygon();
+        float startX = Float.parseFloat(shapePoints[0]);
+        float startY = Float.parseFloat(shapePoints[1]);
+        // The first 2 points in the MapShapeData will be (0, 0) because its x and y properties
+        // will be set to the first 2 points we read from input
+        xPoints.add(0f);
+        yPoints.add(0f);
+        
+        try{
+            // Continuously add points 
+            for (int i = 2; i < shapePoints.length; i += 2) {
+                float x = Float.parseFloat(shapePoints[i]);
+                float y = Float.parseFloat(shapePoints[i + 1]);
+                xPoints.add(x);
+                yPoints.add(y);
+            }
+        } catch (Exception e){
+            System.err.println(e.getMessage());
+        }
 
-        return country;
+        MapShapeData mapShape = new MapShapeData(startX, startY, xPoints.size(), relativePoints);
+        // Move all the data from the lists to the array in MapShapeData
+        for (int i = 0; i < mapShape.numPoints; i++) {
+            mapShape.setPoint(i, xPoints.get(i), yPoints.get(i));
+        }
+
+        // Create renderable Polygon object
+        mapShape.updatePolygon();
+
+        return mapShape;
     }
 
     public static List<MapShapeData> parseWorldMapFile(Scanner input) {
         List<MapShapeData> mapShapes = new ArrayList<MapShapeData>();
 
         while (input.hasNextLine()) {
-            String nextShape = input.nextLine();
-            Scanner shapeScanner = new Scanner(nextShape);
-
-            List<Float> xPoints = new ArrayList<Float>();
-            List<Float> yPoints = new ArrayList<Float>();
-
-            // The first 2 numbers will be interpreted as the starting position and the first point in the shape
-            // After that, we will continuously add points
-            float startX = shapeScanner.nextFloat();
-            float startY = shapeScanner.nextFloat();
-
-            xPoints.add(0f);
-            yPoints.add(0f);
-            
-            while (shapeScanner.hasNext()) {
-                float posX = shapeScanner.nextFloat();
-                float posY = shapeScanner.nextFloat();
-
-                posX -= startX;
-                posY -= startY;
-
-                xPoints.add(posX);
-                yPoints.add(posY);
+            String shape = input.nextLine();
+            switch (shape.charAt(0)) {
+                case '#':
+                    // Comment, ignore
+                    continue;
+                case 'R':
+                    mapShapes.add(parsePolygon(shape.substring(1), true));
+                    break;
+                case 'A':
+                    mapShapes.add(parsePolygon(shape.substring(1), false));
+                    break;
+                default:
+                    // Skip this line. Absolute or relative points must be specified
+                    continue;
             }
-
-            shapeScanner.close();
-
-            // Move all the data from the lists to a new array
-            MapShapeData mapShape = new MapShapeData(startX, startY, xPoints.size(), false);
-            mapShape.xPoints = new float[mapShape.numPoints];
-            mapShape.yPoints = new float[mapShape.numPoints];
-            for (int i = 0; i < mapShape.numPoints; i++) {
-                mapShape.setPoint(i, xPoints.get(i), yPoints.get(i));
-            }
-
-            mapShape.updatePolygon();
-
-            mapShapes.add(mapShape);
         }
 
         return mapShapes;
